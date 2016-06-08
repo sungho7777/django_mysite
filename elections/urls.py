b8b8fabf3c2bd9946734ab54d73317016a7b3c41
SUNGHO7777 <mzncvmc@gmail.com>
2016-06-08 10:46:07 +0900

    20160608 장고를 활용한 웹사이트 만들기

---
+++ elections/urls.py
@@ -0,0 +1,11 @@
+﻿from django.conf.urls import url
+from . import views
+
+app_name = 'elections'
+urlpatterns = [
+    url(r'^$', views.index, name = 'home'),
+    url(r'^areas/(?P<area>[가-힣]+)/$', views.areas), #수정
+    url(r'^areas/(?P<area>[가-힣]+)/results$', views.results), #추가. 꼭 r'^areas/(?P<area>[가-힣]+)/$' 보다 밑에 적어주세요.
+    url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
+	url(r'^candidates/(?P<name>[가-힣]+)/$', views.candidates),
+]
